8531b319884d2cf4591966b143d72515ad654d34
Taras Makarchuk <2278452@gmail.com>
2023-06-06 18:48:21 +0300

    update order.service.ts file

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class OrderService {
