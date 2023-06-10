e10957057eac6b4171e9e590699f09aa866fe93b
Taras Makarchuk <2278452@gmail.com>
2023-06-10 20:58:44 +0300

    update goods.module.ts file

--- src/goods/goods.module.ts
+++ src/goods/goods.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { GoodsService } from './goods.service';
 import { GoodsController } from './goods.controller';
-import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [GoodsController],
   providers: [GoodsService, PrismaService],
 })
-export class GoodsModule {}
+export class GoodsModule { }
