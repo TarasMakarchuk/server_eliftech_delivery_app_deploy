17d242254b7f1001712c9dab38b512621ef46876
Taras Makarchuk <2278452@gmail.com>
2023-06-12 20:57:41 +0300

    update app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { GoodsModule } from './goods/goods.module';
 import { ConfigModule } from '@nestjs/config';
+import { GoodsModule } from './goods/goods.module';
 import { ShopModule } from './shop/shop.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { OrderModule } from './order/order.module';
