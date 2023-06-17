6adee404f8f5c9e404bd92901cf9858f07d0195f
Taras Makarchuk <2278452@gmail.com>
2023-06-17 16:58:03 +0300

    update order.module.ts file

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [OrderController],
   providers: [OrderService, PrismaService],
 })
-export class OrderModule {}
+export class OrderModule { }
