493a94a786fbeeb5d44ac1551310985bb1c2afa5
Taras Makarchuk <2278452@gmail.com>
2023-06-28 23:20:43 +0300

    update main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
