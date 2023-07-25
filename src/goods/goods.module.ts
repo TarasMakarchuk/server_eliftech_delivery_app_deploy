cdf4f78bbf097367e54e53d3d45ffd16cfa61e8e
Taras Makarchuk <2278452@gmail.com>
2023-07-25 22:11:58 +0300

    update goods.module.ts file

--- src/goods/goods.module.ts
+++ src/goods/goods.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { GoodsService } from './goods.service';
+import { PrismaService } from '../prisma.service';
 import { GoodsController } from './goods.controller';
 
 
